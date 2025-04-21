f91dd7df41d0423cc65ceb97f6d6b0a907c88bbd
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-04-21 21:22:01 +0700

    add image & button

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, Button } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Image source={require("../assets/images/react-logo.png")} />
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button
+        title="Click Me"
+        onPress={() => {
+          alert("Hello World");
+        }}
+      />
     </View>
   );
 }
